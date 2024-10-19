079a6741af8ed58e67c0cac52c8f3a1d8be34a43
matiasmenesess <155995043+matiasmenesess@users.noreply.github.com>
2024-10-19 16:29:16 -0500

    Update problema1.cpp

--- problema1.cpp
+++ problema1.cpp
@@ -1,3 +1,7 @@
+//Integrantes:
+//1. Olivares Sarmiento, Javier Ignacio Leon
+//2. Silva Reyes, Santiago Miguel
+//3. Meneses Roncal, Matias Alonso
 // C++ implementation of search() and traverse() methods
 #include <iostream>
 using namespace std;
