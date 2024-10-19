32c0bc3f5c198848a421998e4e3252064ddd7409
matiasmenesess <155995043+matiasmenesess@users.noreply.github.com>
2024-10-19 17:01:06 -0500

    Update problema2.cpp

--- problema2.cpp
+++ problema2.cpp
@@ -34,7 +34,7 @@ public:
 
 // Make BTree friend of this so that we can access private members of this
 // class in BTree functions
-friend class BTree;
+    friend class BTree;
 };
 
 // A BTree
@@ -62,32 +62,32 @@ public:
 // Constructor for BTreeNode class
 BTreeNode::BTreeNode(int t1, bool leaf1)
 {
-    
+
     t = t1;
     leaf = leaf1;
 
-    
+
     keys = new int[2*t-1];
     C = new BTreeNode *[2*t];
 
-    
+
     n = 0;
 }
 
 // Function to traverse all nodes in a subtree rooted with this node
 void BTreeNode::traverse()
 {
-    
+
     int i;
     for (i = 0; i < n; i++)
     {
-        
+
         if (leaf == false)
             C[i]->traverse();
         cout << " " << keys[i];
     }
 
-    
+
     if (leaf == false)
         C[i]->traverse();
 }
@@ -95,27 +95,35 @@ void BTreeNode::traverse()
 
 BTreeNode *BTreeNode::search(int k)
 {
-    
+
     int i = 0;
     while (i < n && k > keys[i])
         i++;
 
-   
+
     if (keys[i] == k)
         return this;
 
-    
+
     if (leaf == true)
         return NULL;
 
-    
+
     return C[i]->search(k);
 }
 
 // The main function that inserts a new key in this B-Tree
 void BTree::insert(int k)
 {
- // !!!YOUR CODE
+    if(root ==NULL){
+        auto* NewNode = new BTreeNode(t, true);
+        NewNode->keys[0] = k;
+        NewNode->n = NewNode->n + 1;
+        root = NewNode;
+    }
+    else{
+
+    }
 }
 
 // A utility function to insert a new key in this node
@@ -123,33 +131,33 @@ void BTree::insert(int k)
 // function is called
 void BTreeNode::insertNonFull(int k)
 {
-    
+
     int i = n-1;
 
-    
+
     if (leaf == true)
     {
-        
+
         while (i >= 0 && keys[i] > k)
         {
             keys[i+1] = keys[i];
             i--;
         }
 
-        
+
         keys[i+1] = k;
         n = n+1;
     }
-    else 
+    else
     {
-        
+
         while (i >= 0 && keys[i] > k)
             i--;
 
-        
+
         if (C[i+1]->n == 2*t-1)
         {
-            
+
             splitChild(i+1, C[i+1]);
 
             if (keys[i+1] < k)
@@ -163,39 +171,39 @@ void BTreeNode::insertNonFull(int k)
 // Note that y must be full when this function is called
 void BTreeNode::splitChild(int i, BTreeNode *y)
 {
-    
+
     BTreeNode *z = new BTreeNode(y->t, y->leaf);
     z->n = t - 1;
 
-    
+
     for (int j = 0; j < t-1; j++)
         z->keys[j] = y->keys[j+t];
 
-    
+
     if (y->leaf == false)
     {
         for (int j = 0; j < t; j++)
             z->C[j] = y->C[j+t];
     }
 
-    
+
     y->n = t - 1;
 
-    
+
     for (int j = n; j >= i+1; j--)
         C[j+1] = C[j];
 
-    
+
     C[i+1] = z;
 
-    
+
     for (int j = n-1; j >= i; j--)
         keys[j+1] = keys[j];
 
-    
+
     keys[i] = y->keys[t-1];
 
-    
+
     n = n + 1;
 }
 
